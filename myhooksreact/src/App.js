b3b53d0ba27b5471aa018f2ad3cecee2ec3e77fb
xuan dang <dangkvh@gmail.com>
2023-07-05 14:17:03 +0700

    update App.js

--- myhooksreact/src/App.js
+++ myhooksreact/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-const App =() => () {
+const App =() => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hello ReactJS</h1>>
+        <h1> Hello ReactJS</h1>
       </header>
     </div>
   );
