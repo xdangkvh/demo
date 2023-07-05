cb322a3a19a001a88e9d93a47e1435cf224060ed
ADMIN <dangkvh@gmail.com>
2023-07-05 22:26:11 +0700

    event onclick

--- myhooksreact/src/App.js
+++ myhooksreact/src/App.js
@@ -2,19 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
 
-const App =() =>  {
+const App = () => {
   let name = "Truong";
-  let number = 2021; 
-  let obj = {name : "Dang", age: 27}
-  let link = "https://www.youtube.com/watch?v=HI3cFTFdOPk&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=13&ab_channel=H%E1%BB%8FiD%C3%A2nIT"
+
+  const handleEventClick = (event) => {
+    console.log("Click me", event.target.value);
+  }
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hello ReactJS {name} in {number}</h1>
-        <p>{JSON.stringify(obj)}</p>
-        <a href = {link} target = "_blank"> Visit my channel</a>
+        <h1> Hello ReactJS {name} </h1>
+        <input type="text" value="Dang" onClick={(event) => handleEventClick(event)} />
+        <button type="button" onClick={(event) => handleEventClick(event)}> Click me</button>
       </header>
     </div>
   );
