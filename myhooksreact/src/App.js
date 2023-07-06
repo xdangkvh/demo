b939b229545a1560f4ccb2b86af94b3cfff56e90
xuan dang <dangkvh@gmail.com>
2023-07-06 09:51:36 +0700

    Output List

--- myhooksreact/src/App.js
+++ myhooksreact/src/App.js
@@ -6,10 +6,23 @@ const App = () => {
   // let name = "Truong";
   const [name, setName] = useState("Dang");
   const [address, setAddress] = useState();
+  const [todos, setTodos] = useState([
+    {id: "to do1", title:"Watching movie"},
+    {id: "to do2", title: "Hello World"},
+    {id: "tod do3", title:"playing game"}
+  ]);
+
   const handleEventClick = (event) => {
-    setName(address);
-    console.log(address)
+    // setName(address);
+    // console.log(address)
     // console.log("Click me", {name});
+    if(!address){
+      alert("Empty input")
+      return
+    }
+    let newTodo = {id: "4", title:address}
+    setTodos([...todos, newTodo])
+    setAddress("")
   }
   const handleOnChange = (event) =>{
     setAddress(event.target.value)
@@ -21,6 +34,19 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Hello ReactJS {name} </h1>
+        <div className='todos-container' >
+            {
+              todos.map(todo => {
+                return(
+                  <li className='todo-child' key={todo.id}>
+                    {todo.title}
+                  </li>
+                )
+              })
+            }
+         
+
+        </div>
         <input type="text" value={address} onChange={(event) => handleOnChange(event)} />
         <button type="button" onClick={(event) => handleEventClick(event)}> Click me</button>
       </header>
