9fd0c314f01d6dbcd79e607966521fed40cf4f77
ADMIN <dangkvh@gmail.com>
2023-07-10 22:22:37 +0700

    router hooks

--- myhooksreact/src/views/Nav.js
+++ myhooksreact/src/views/Nav.js
@@ -1,14 +1,14 @@
 import '../views/Nav.scss'
-const Nav = () =>{
-    return(
+const Nav = () => {
+    return (
 
         <div className="topnav">
             <a className="active" href="#home">Home</a>
-            <a href="#news">News</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
+            <a href="/timer">Timer Apps</a>
+            <a href="/todo">Todo Apps</a>
+            <a href="/secret">Secrets</a>
         </div>
-     
+
     );
 }
 export default Nav;
