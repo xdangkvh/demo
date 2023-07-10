9681354d6e50b59541215f56e44da202e49508fb
ADMIN <dangkvh@gmail.com>
2023-07-10 20:43:50 +0700

    effect hooks

--- myhooksreact/src/views/Covid.js
+++ myhooksreact/src/views/Covid.js
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-const Covid = () =>{
+const Covid = () => {
 
-    useEffect (() => {
-        axios.get("https://api.covid19api.com/country/vietnam")
-    }, [])
-    return(
-        <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-      </table>
-    )
+  useEffect(async () => {
+    let res = await axios.get("https://api.covid19api.com/country/vietnam")
+    console.log("check res:", res)
+  }, [])
+  return (
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+    </table>
+  )
 }
 export default Covid; 
